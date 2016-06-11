208f9158ff2cc252dd28f86318cfa0be0a8f04be
kaelzhang81 <zhangy1981@qq.com>
2016-06-12 00:04:06 +0800

    add category controller

---
+++ controllers/category.go
@@ -0,0 +1,13 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type CategoryController struct {
+	beego.Controller
+}
+
+func (this *CategoryController) Get() {
+	this.TplName = "category.html"
+}
