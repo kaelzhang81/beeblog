209e31156578c8c5d898da475836cec53207bc60
kaelzhang81 <zhangy1981@qq.com>
2016-06-16 22:39:27 +0800

    add topic get & modify & delete function

--- models/models.go
+++ models/models.go
@@ -60,19 +60,6 @@ func RegisterDB() {
 	orm.RegisterDataBase("default", _SQLITE3_DRIVER, _DB_NAME, 10)
 }
 
-func AddTopic(title, content string) error {
-	o := orm.NewOrm()
-
-	topic := &Topic{
-		Title:   title,
-		Content: content,
-		Created: time.Now(),
-		Updated: time.Now(),
-	}
-	_, err := o.Insert(topic)
-	return err
-}
-
 func AddCategory(name string) error {
 	o := orm.NewOrm()
 
@@ -90,11 +77,8 @@ func AddCategory(name string) error {
 	}
 
 	_, err = o.Insert(cate)
-	if err != nil {
-		return err
-	}
 
-	return nil
+	return err
 }
 
 func DeleteCategory(id string) error {
@@ -110,6 +94,78 @@ func DeleteCategory(id string) error {
 	return err
 }
 
+func GetAllCategories() ([]*Category, error) {
+	o := orm.NewOrm()
+	cates := make([]*Category, 0)
+
+	qs := o.QueryTable("category")
+	_, err := qs.All(&cates)
+	return cates, err
+}
+
+func AddTopic(title, content string) error {
+	o := orm.NewOrm()
+
+	topic := &Topic{
+		Title:   title,
+		Content: content,
+		Created: time.Now(),
+		Updated: time.Now(),
+	}
+	_, err := o.Insert(topic)
+	return err
+}
+
+func GetTopic(tid string) (*Topic, error) {
+	tidNum, err := strconv.ParseInt(tid, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	o := orm.NewOrm()
+	topic := new(Topic)
+
+	qs := o.QueryTable("topic")
+	err = qs.Filter("id", tidNum).One(topic)
+	if err != nil {
+		return nil, err
+	}
+
+	topic.Views++
+	_, err = o.Update(topic)
+	return topic, nil
+}
+
+func ModifyTopic(tid, title, content string) error {
+	tidNum, err := strconv.ParseInt(tid, 10, 64)
+	if err != nil {
+		return err
+	}
+
+	o := orm.NewOrm()
+	topic := &Topic{Id: tidNum}
+	if o.Read(topic) != nil {
+		topic.Title = title
+		topic.Content = content
+		topic.Updated = time.Now()
+		_, err = o.Update(topic)
+	}
+
+	return err
+}
+
+func DeleteTopic(tid string) error {
+	tidNum, err := strconv.ParseInt(tid, 10, 64)
+	if err != nil {
+		return err
+	}
+
+	o := orm.NewOrm()
+	topic := &Topic{Id: tidNum}
+	_, err = o.Delete(topic)
+	return err
+}
+
 func GetAllTopics(isDesc bool) (topics []*Topic, err error) {
 	o := orm.NewOrm()
 
@@ -122,13 +178,4 @@ func GetAllTopics(isDesc bool) (topics []*Topic, err error) {
 		_, err = qs.All(&topics)
 	}
 	return topics, err
-}
-
-func GetAllCategories() ([]*Category, error) {
-	o := orm.NewOrm()
-	cates := make([]*Category, 0)
-
-	qs := o.QueryTable("category")
-	_, err := qs.All(&cates)
-	return cates, err
 }
