7cdf8402746cc9c72c9b84837369e55ab7b39aaa
kaelzhang81 <zhangy1981@qq.com>
2016-06-25 07:33:35 +0800

    upload attachment

---
+++ attachment/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+	//	"beeblog/controllers"
+	"beeblog/models"
+	"beeblog/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+)
+
+func init() {
+	// 注册数据库
+	models.RegisterDB()
+}
+
+func main() {
+	// 开启 ORM 调试模式
+	orm.Debug = true
+	// 自动建表
+	orm.RunSyncdb("default", false, true)
+
+	os.Mkdir("attachment", os.ModePerm)
+
+	// 注册 beego 路由
+	routers.Init()
+
+	// 启动 beego
+	beego.Run()
+}
